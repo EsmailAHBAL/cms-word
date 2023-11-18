769de487ce68e2cf4246078a38b0453a600a05e6
esmail <usmail.developer@gmail.com>
2023-11-18 19:01:05 +0100

    adding new logo

--- components/alert.tsx
+++ components/alert.tsx
@@ -26,7 +26,7 @@ export default function Alert({ preview }) {
 
                   <Link href={"/"} className="font-extrabold md:p-4 p-2 text-xl md:text-lg text-black">Smart Mini
                     <span className="text-gray-800 pl-1">Car</span> </Link>
-                  <Link href={"/"} className="text-black"><Image src={'/logo.png'} height={80} width={80} alt='n;o ' priority></Image></Link>
+                  <Link href={"/"} className="text-black"><Image src={'/logo.png'} height={50} width={50} alt='n;o ' priority></Image></Link>
                 </div>            </>
             ) : (
               <>
@@ -34,7 +34,7 @@ export default function Alert({ preview }) {
 
                   <Link href={"/"} className="font-extrabold md:p-4 p-2 text-xl md:text-2xl text-black ">Smart Mini
                     <span className="text-gray-900  pl-1">Car</span> </Link>
-                  <Link href={"/"} className="text-black"><Image src={'/logo.png'} height={100} width={100} alt='n;o
+                  <Link href={"/"} className="text-black"><Image src={'/logo.png'} height={50} width={50} alt='n;o
               
               '
                     className="object-cover object-center w-full h-full"
