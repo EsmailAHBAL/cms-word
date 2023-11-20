d55f180a3d1756875f4ed7fb7e2c1ffc6cdf9584
esmail <usmail.developer@gmail.com>
2023-11-20 22:55:38 +0100

    adding font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,30 +8,29 @@ import AppContext from '../lib/app.context'
 import { getAllCategories, getAllCategoriesOfCars, getAllPostsForHome } from '../lib/api'
 import preview from './api/preview'
 import 'bulma/css/bulma.css';
-import { IBM_Plex_Mono} from 'next/font/google'
-const Font = IBM_Plex_Mono ({
-weight:"400",subsets:['cyrillic-ext']
+import { Josefin_Sans } from 'next/font/google'
+const Font = Josefin_Sans({
+  weight: "400", subsets: ['latin-ext']
 })
-function MyApp({ Component, pageProps ,data  }) 
-{
-  return <Provider store={store}> 
-  <AppContext.Provider value={{categories : data}}>
-<div className={Font.className}>
-  <Component {...pageProps} />
+function MyApp({ Component, pageProps, data }) {
+  return <Provider store={store}>
+    <AppContext.Provider value={{ categories: data }}>
+      <div className={Font.className}>
+        <Component {...pageProps} />
 
-</div>
+      </div>
 
-  </AppContext.Provider>
+    </AppContext.Provider>
   </Provider>
 }
 MyApp.getInitialProps = async (_ctx: NextPageContext) => {
-  const all :any []= await getAllCategories(preview)
-const cars = await getAllCategoriesOfCars(preview)
-const sidePost = await getAllPostsForHome(preview) 
+  const all: any[] = await getAllCategories(preview)
+  const cars = await getAllCategoriesOfCars(preview)
+  const sidePost = await getAllPostsForHome(preview)
   console.log(cars);
-  
-  return { data:{all,cars,sidePost :  sidePost.edges.slice(6)} }
+
+  return { data: { all, cars, sidePost: sidePost.edges.slice(6) } }
 }
- 
+
 
 export default MyApp
